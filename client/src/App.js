e13245de83331201a6bf4edf708b7d699a201c36
Huan <huan.zeng1@gmail.com>
2022-04-05 15:48:24 -0400

    added homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,17 @@
-function App() {
-  return <div className="App"></div>
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+
+const App = () => {
+  return (
+    <div className="App">
+      <header></header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+    </div>
+  )
 }
 
 export default App
